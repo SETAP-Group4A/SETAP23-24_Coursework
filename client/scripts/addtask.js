95b42d88af49f45986637474be789978f009cbe0
fl720 <fl720@ic.ac.uk>
2024-05-01 17:28:18 +0100

    add autoscheduler function to addtask module

--- client/scripts/addtask.js
+++ client/scripts/addtask.js
@@ -24,3 +24,39 @@ function saveTaskToLocalStorage(task) {
     localStorage.setItem("tasks", JSON.stringify(tasks))
     window.location = "/dashboard";
 }
+
+function autoScheduler(newTask) {
+    const ExistingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const ExistingTasksFromDate = [];
+    for (let i = 0; i < ExistingTasks.length; i++) {
+      const task = ExistingTasks[i];
+      if (task.date === newTask.date) {
+        ExistingTasksFromDate.push(task);
+      }
+    }
+    const OverlappingTasks = [];
+    for (let i = 0; i < ExistingTasksFromDate.length; i++) {
+      const task = ExistingTasksFromDate[i];
+      if (
+        newTask.startTime >= task.startTime &&
+        newTask.startTime < task.endTime
+      ) {
+        OverlappingTasks.push(task);
+        return true;
+      } else if (
+        newTask.endTime > task.startTime &&
+        newTask.endTime <= task.endTime
+      ) {
+        OverlappingTasks.push(task);
+        return true;
+      } else if (
+        newTask.startTime <= task.startTime &&
+        newTask.endTime >= task.endTime
+      ) {
+        OverlappingTasks.push(task);
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
