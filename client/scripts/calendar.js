84b950b2bf029672c1477127b79f47c9dbde641b
George Lazo <misterzipzap@gmail.com>
2024-05-03 17:08:45 +0100

    toggle password visibility button, tasks are clickable need to addd html element

--- client/scripts/calendar.js
+++ client/scripts/calendar.js
@@ -1,9 +1,33 @@
+function displayTaskInfo(event) {
+  //Code for going to smaller window
+
+  //Get Handles
+  let taskName = document.getElementById('taskName');
+  let taskDate = document.getElementById('taskDate');
+  let taskStartTime = document.getElementById('taskStartTime');
+  let taskEndTime = document.getElementById('taskEndTime');
+  let taskPriority = document.getElementById('taskPriority');
+  let taskDesc = document.getElementById('taskDesc');
+  let back = document.getElementById('back');
+  back.addEventListener('click', () => {});
+
+  //Set the values
+  taskName.textContent = event.title;
+  taskDate.textContent = event.start;
+  taskStartTime.textContent = event.start;
+  taskEndTime.textContent = event.end;
+  taskPriority.textContent = event.backgroundColor;
+  taskDesc.textContent = event.desc;
+}
+
 function LoadCalendar() {
   // Set the desired width and height for the calendar
   const calendarWidth = "100%";
   const calendarHeight = "600px";
-  const slotMinTime = "08:00:00"; // Example start time
-  const slotMaxTime = "18:00:00"; // Example end time
+  //const slotMinTime = "08:00:00"; // Example start time
+  //const slotMaxTime = "18:00:00"; // Example end time
+
+  
 
   let events = LoadTasks();
 
@@ -12,14 +36,18 @@ function LoadCalendar() {
     initialView: "timeGridThreeDay",
     height: calendarHeight,
     width: calendarWidth,
-    slotMinTime: slotMinTime,
-    slotMaxTime: slotMaxTime,
+    //slotMinTime: slotMinTime,
+    //slotMaxTime: slotMaxTime,
     headerToolbar: {
       left: "prev,next",
       center: "title",
       right: "timeGridThreeDay,timeGridWeek,dayGridMonth",
     },
     events: events,
+    eventClick: function (info) {
+      events.forEach(event => {
+        displayTaskInfo(event)});
+    },
     views: {
       timeGridThreeDay: {
         type: "timeGrid",
@@ -62,7 +90,6 @@ function LoadTasks() {
               let color = priorityColors[task.prio] || '#3377FF'; // Default color if priority not found
               event.backgroundColor = color;
           }
-
           events.push(event);
       });
   }
